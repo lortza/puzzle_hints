b7f5231111f8d23372926f25cb4cc970473a6f24
Anne Richardson <richardson.ae@gmail.com>
2024-07-21 16:17:45 -0400

    Adapt spellling_bee code

--- models/spelling_bee.rb
+++ models/spelling_bee.rb
@@ -1,14 +1,22 @@
 require_relative 'dictionary'
 
-module SpellingBee
-  def self.spelling_bee(must_contain:, can_contain:)
-    containable_letters = (can_contain << must_contain)
-    selected_words = ::Dictionary.wordle_possible_answers.select do |word|
-      word.chomp!
-      next if word.length < 4
+class SpellingBee
+  def initialize(must_contain: '', can_contain: '')
+    @must_contain_letter = must_contain.gsub(/[^a-zA-Z]/, '').downcase
+    @can_contain_letters = can_contain.gsub(/[^a-zA-Z]/, '').downcase.chars
+    @containable_letters = @can_contain_letters << @must_contain_letter
+  end
+
+  def suggest
+    selected_words = ::Dictionary.words_with_min_char_count(4).select do |word|
       word_letters = word.chars.uniq
-      word.include?(must_contain) && (word_letters - containable_letters).length.zero?
+      word.include?(@must_contain_letter) && (word_letters - @containable_letters).length.zero?
     end
-    pp selected_words
+    selected_words
   end
 end
+
+# puts SpellingBee.new(
+#   must_contain: 'g',
+#   can_contain: 'eatbmi'
+# ).suggest
