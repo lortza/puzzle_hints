67a2436baaae949dac94d41a07bff3cc0f0c1509
Anne Richardson <richardson.ae@gmail.com>
2024-07-21 08:34:40 -0400

    Build out wordle application

--- application.rb
+++ application.rb
@@ -10,3 +10,61 @@ require_relative 'models/wordle'
 #   "Hello World!"
 #   erb :index
 # end
+
+get '/wordle' do
+  # Get all posts from database
+  # @posts = Post.all
+
+  # Render posts index view
+  erb :'wordle/index'
+end
+
+get '/', '/wordle/suggestions' do
+  # placeholder_character = "_"
+  @letter_1 = params[:letter_1]
+  @letter_2 = params[:letter_2]
+  @letter_3 = params[:letter_3]
+  @letter_4 = params[:letter_4]
+  @letter_5 = params[:letter_5]
+
+  incoming_letters_blank = [@letter_1, @letter_2, @letter_3, @letter_4, @letter_5].compact.empty?
+  if incoming_letters_blank
+    @suggestions = []
+  else
+    word_w_placeholders = [
+      letter_or_placeholder(@letter_1),
+      letter_or_placeholder(@letter_2),
+      letter_or_placeholder(@letter_3),
+      letter_or_placeholder(@letter_4),
+      letter_or_placeholder(@letter_5)
+    ].join
+
+    @required_letters = params[:required_letters].nil? ? "" : params[:required_letters]
+    @excluded_letters = params[:excluded_letters].nil? ? "" : params[:excluded_letters]
+    @suggestions = Wordle.new(
+      word_with_placeholders: word_w_placeholders,
+      required_letters: @required_letters.chars,
+      excluded_letters: @excluded_letters.chars,
+      placeholder_character: Wordle::PLACEHOLDER_CHARACTER
+    ).suggest
+    # @suggestions = Wordle.temp_list
+  end
+
+  erb :'wordle/suggestions'
+end
+
+post '/posts' do
+  # Get posted form data
+  title = params[:title]
+  body = params[:body]
+
+  # Create post in database
+  # @post = Post.create(title: title, body: body)
+
+  # Redirect to post page
+  # redirect "/posts/#{@post.id}"
+end
+
+def letter_or_placeholder(value)
+  value.nil? || value&.empty? ? Wordle::PLACEHOLDER_CHARACTER : value
+end
---
+++ models/wordle.rb
@@ -0,0 +1,80 @@
+require_relative 'dictionary'
+
+class Wordle
+  PLACEHOLDER_CHARACTER = "_"
+  def initialize(word_with_placeholders:, excluded_letters:, required_letters: nil, placeholder_character: PLACEHOLDER_CHARACTER)
+    @submitted_word_with_placeholders = word_with_placeholders.upcase
+    @excluded_letters = excluded_letters.map(&:upcase)
+    @required_letters = required_letters&.map(&:upcase)
+    @available_letters = ('A'..'Z').to_a - @excluded_letters
+    @submitted_placeholder_character = placeholder_character
+  end
+
+  def temp_list
+    %w[a b c d e f]
+  end
+
+  def suggest2
+    binding.pry
+  end
+
+  def suggest
+    # raise 'Must have at least 2 known letters' if @submitted_word_with_placeholders.count(@submitted_placeholder_character) > 3 && @excluded_letters.length < 10
+
+    built_words = []
+    # available_letters = available_letters.map(&:upcase)
+    words_with_placeholders = [@submitted_word_with_placeholders]
+    # While this array of words has at least one presence of _
+    while words_with_placeholders.length > 0
+      # Loop over each word...
+      words_with_placeholders.each do |word|
+        # Skip if the word does not have the placeholder character...
+        next unless word.include?(@submitted_placeholder_character)
+
+        # For each word, replace the first instance of the placeholder...
+        word.chars.each_with_index do |word_letter, index|
+          # Skip this letter if it is alpha...
+          next unless word_letter == @submitted_placeholder_character
+
+          @available_letters.each do |available_letter|
+            available_letter
+            word[index] = available_letter
+            if word.include?(@submitted_placeholder_character)
+              words_with_placeholders << word.dup
+            elsif @required_letters
+              (built_words << word.dup) if @required_letters.all? {|l| word.include?(l)}
+            else
+               built_words << word.dup
+            end
+          end # each available letter
+          # remove original word from words_with_placeholders array
+          words_with_placeholders.delete(word)
+        end # each placeholder char
+      end # words_with_placeholders.each
+    end # while
+
+    # Return only the words that appear in both the dictionary and the built words
+    results = ::Dictionary.wordle_possible_answers & built_words
+    # puts results
+    # puts "-------"
+    # puts "#{results.length} words found"
+    # puts ""
+    # puts 'FINISHED'
+  end
+end
+
+# Instant answer
+# puts Wordle.new(
+#   word_with_placeholders: 'sha_t',
+#   required_letters: ''.chars,
+#   excluded_letters: 'lcr'.chars,
+#   placeholder_character: '_'
+# ).suggest
+
+# Takes about 1 minute
+# puts Wordle.new(
+#   word_with_placeholders: '_____',
+#   required_letters: 'e'.chars,
+#   excluded_letters: 'bornmail'.chars,
+#   placeholder_character: '_'
+# ).suggest
